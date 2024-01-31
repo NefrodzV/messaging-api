d37e5510b8e36a27eb79c70a0bbbd6339fe33b74
Neftaly <neftalyrv23@gmail.com>
2024-01-31 11:56:36 -0400

    Basic project setup

---
+++ app.js
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app = express()
+
+app.use(express.json())
+
+app.use('/', (req, res) => {
+    res.status(200).json({ msg: "Index api working"})
+})
+
+app.listen(3000, ()=> console.log("Server started in port 3000"))
