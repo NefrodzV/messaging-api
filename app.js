99abbbfec4105ea791c102d49b35a05aa1c5820b
Neftaly <neftalyrv23@gmail.com>
2024-01-31 12:32:39 -0400

    Added files for routes, controllers and models

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ import express from 'express'
 
 const app = express()
 
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 app.use('/', (req, res) => {
     res.status(200).json({ msg: "Index api working"})
 })
 
-app.listen(3000, ()=> console.log("Server started in port 3000"))
+app.listen(3000, () => console.log("Server started in port 3000"))
