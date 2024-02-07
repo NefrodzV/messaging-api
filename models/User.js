d5b6f9924b2430b606707c3a9bd7063c93e9c044
Neftaly <neftalyrv23@gmail.com>
2024-02-07 14:47:24 -0400

    made email unique

--- models/User.js
+++ models/User.js
@@ -6,7 +6,7 @@ const userSchema = new Schema({
     profile: {
         username: { type: String, required: true },
         password: { type: String, required:true },
-        email: { type: String, required:true },
+        email: { type: String, required:true, unique: true },
         image: { 
             name: String, 
             mimeType: String, 
