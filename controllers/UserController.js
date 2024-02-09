df174cd74a0eb047ff983be034a1037110d08204
Neftaly <neftalyrv23@gmail.com>
2024-02-09 16:02:23 -0400

    Made logic to get user if token is correct

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,9 +1,43 @@
-
+import jwt from 'jsonwebtoken'
+import { User } from '../models'
 function UserController() {
 
-    const getUser =  async (req, res) => {
-        res.send("Get user not implemented")
-    }
+    // Handles the chat creation of user and 
+    const getUser = [
+        async function verifyToken(req, res, next) {
+            const bearerHeader = req.headers['authorization']
+            console.log(bearerHeader)
+            if(bearerHeader) {
+                const bearer = bearerHeader.split(' ')
+                const token = bearer[1]
+                if(!token){
+                    res.status(403).json({ msg: "Forbidden"})
+                    return
+                }
+                req.token = token
+                next()
+            } else {
+                res.status(403).json({ msg: "Forbidden" })
+            }
+        },
+
+        async (req, res) => {
+            // Processing the token data
+            jwt.verify(req.token, process.env.TOKEN_SECRET, async (err, data) => {
+                if(err) throw new Error('Decode error')
+                if(!data) {
+                    res.status(403).json({ msg: "Forbidden" })
+                }
+
+                const user = await User.findById(data.id, {
+                    "profile.username": 1,
+                    "profile.image": 1
+                }
+                )
+                res.status(200).json({user: user})
+            })
+        }
+    ]
     
     const getChats = (req, res) => {
         res.send("Get all user chats not implemented")
