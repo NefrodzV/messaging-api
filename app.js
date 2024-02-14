4ec9076628ba00861c4e29065d09bb85f1796ef1
Neftaly <neftalyrv23@gmail.com>
2024-02-14 16:01:32 -0400

    Set up routers for express

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import { SessionRouter, UserRouter } from './routes/index.js'
 
 const app = express()
 
@@ -7,6 +8,32 @@ app.use(express.json())
 
 app.use('/', (req, res) => {
     res.status(200).json({ msg: "Index api working"})
+})
+app.use('/api/session', SessionRouter)
+app.use('/api/users',  UserRouter)
+
+// Error handling globally
+app.use((err, req, res, next) => {
+    if(err instanceof JsonWebTokenError) {
+        res.status(403).json({ errors: {
+            authorization: 'Forbidden'
+        }})
+        return
+    }
+
+    if(err instanceof mongo.MongoServerError) {
+        res.status(500).json({ 
+            errors: {
+                database: "Something went wrong with the database"
+            }
+        })
+        return
+    }
+    
+    // Any unhandled error
+    res.status(500).json({
+        message: "Something went wrong with the server"
+    })
 })
 
 app.listen(3000, () => console.log("Server started in port 3000"))
