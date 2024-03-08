7c8e3d35b9d29d24948999ef306f542ec7deaabe
Neftaly <neftalyrv23@gmail.com>
2024-03-07 20:24:27 -0400

    Made two routes to change password and image profile

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -6,5 +6,7 @@ const router = Router()
 router.get('/', userController.getUsers)
 // Me route is for user that began a session with token
 router.get('/me', userController.getUser)
+router.put('/me/password', userController.changePassword)
+router.put("/me/image", userController.uploadProfileImage)
 
 export default router
