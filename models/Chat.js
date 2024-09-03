5cd7a2a1f7e0e722e6e476b3bea81d9af309a8f1
Neftaly <neftalyrv23@gmail.com>
2024-09-03 12:41:12 -0400

    removed unnecessary  fields

--- models/Chat.js
+++ models/Chat.js
@@ -3,9 +3,7 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema
 
 const chatSchema = new Schema({
-    lastMessage: { type: Schema.Types.ObjectId, ref: 'Message'},
     users: [{type: Schema.Types.ObjectId , ref: "User"}],
-    messages:[{type: Schema.Types.ObjectId, ref: "Message"}]
 })
 
 const Chat = mongoose.model('Chat', chatSchema)
