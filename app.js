752bf03b015dc8f7c86cdd21af6d4b8c4af5ef38
Neftaly <neftalyrv23@gmail.com>
2024-09-10 16:11:00 -0400

    made the logic to send back a message or delete a message from a room if a user deleted or edited

--- app.js
+++ app.js
@@ -77,6 +77,31 @@ io.on('connection', (socket) => {
     socket.on('join', (roomId) => {
         socket.join(roomId)
         console.log('Someone join room : ' + roomId)
+        // io.to(roomId).emit('message', {
+        //     _id: 'post101',
+        //     user: {
+        //         username: 'john_doe',
+        //     },
+        //     text: 'Just had a great day at the park!',
+        //     date: '2024-09-04T10:30:00Z',
+        //     imgs: [
+        //         'https://example.com/images/park1.jpg',
+        //         'https://example.com/images/park2.jpg',
+        //     ],
+        // },)
+    })
+
+    socket.on('edit',(roomId, message) => {
+        console.log('editing message from roomId: ' + roomId )
+        console.log(message)
+        io.to(roomId).emit('edit', message)
+    })
+
+    socket.on('delete',(roomId, message) => {
+        console.log('deleting message on roomId: ' + roomId)
+        console.log(message)
+        io.to(roomId).emit('delete', message)
+
     })
 
     socket.on('message',(roomId, text) => {
