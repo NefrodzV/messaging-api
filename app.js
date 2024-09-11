18d91c398e0500bf7c0ed75f7992f0d80cd2303a
Neftaly <neftalyrv23@gmail.com>
2024-09-11 10:02:45 -0400

    refactored the code from soscket into its own file

--- app.js
+++ app.js
@@ -6,18 +6,11 @@ import {mongoose, mongo} from 'mongoose'
 import cors from  'cors'
 import { configDotenv } from 'dotenv'
 import jwt from 'jsonwebtoken'
-import { Server } from 'socket.io'
-import { createServer } from 'node:http'
 import cookieParser from 'cookie-parser'
 configDotenv()
 
-const app = express()
-const server = new createServer(app)
-const io = new Server(server,{
-    cors: {
-        origin: 'http://localhost:5173'
-    }
-})
+export const app = express()
+
 main().catch(e => console.log('Connecting to database error: '+ e))
 const db = mongoose.connection
 db.on('error', () => console.log('db connection failed'))
@@ -68,49 +61,8 @@ app.use((err, req, res, next) => {
         message: "Something went wrong with the server"
     })
 })
-
-server.listen(process.env.PORT , () => console.log("Server started in port 3000"))
-io.on('connection', (socket) => {
-    console.log(socket.id)
-    console.log(socket.request)
-    console.log('user has connected')
-    socket.on('join', (roomId) => {
-        socket.join(roomId)
-        console.log('Someone join room : ' + roomId)
-        // io.to(roomId).emit('message', {
-        //     _id: 'post101',
-        //     user: {
-        //         username: 'john_doe',
-        //     },
-        //     text: 'Just had a great day at the park!',
-        //     date: '2024-09-04T10:30:00Z',
-        //     imgs: [
-        //         'https://example.com/images/park1.jpg',
-        //         'https://example.com/images/park2.jpg',
-        //     ],
-        // },)
-    })
-
-    socket.on('edit',(roomId, message) => {
-        console.log('editing message from roomId: ' + roomId )
-        console.log(message)
-        io.to(roomId).emit('edit', message)
-    })
-
-    socket.on('delete',(roomId, message) => {
-        console.log('deleting message on roomId: ' + roomId)
-        console.log(message)
-        io.to(roomId).emit('delete', message)
-
-    })
-
-    socket.on('message',(roomId, text) => {
-        console.log('Message from roomId: ' + roomId + ' text: ' + text)
-        socket.broadcast.to(roomId).emit('foo',text)
-        // io.emit('foo',text)
-        console.log(socket.id)
-    })
-})
 async function main() {
     await mongoose.connect(process.env.DB_URL)
 }
+
+server.listen(process.env.PORT , () => console.log("Server started in port 3000"))
