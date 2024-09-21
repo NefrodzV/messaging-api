f53f3b3461d40acc4285c953d684726286c62eea
Neftaly <neftalyrv23@gmail.com>
2024-09-21 15:01:06 -0400

    implemented a middleware function for authorization this returns the decode and then add the user to the scoket request. After that upon connection we make a room with the user id to be able to send messages to him. Made the logic to catch a message send from users to specific room and emit a event to both users.

--- socket.js
+++ socket.js
@@ -1,7 +1,29 @@
 import { Server } from 'socket.io';
 import { createServer } from 'node:http';
-
+import { validateAuthCookie } from './utils.js';
+import jwt from 'jsonwebtoken';
+import cookie from 'cookie';
+import { configDotenv } from 'dotenv';
+import User from './models/User.js';
+import Chat from './models/Chat.js';
+import Message from './models/Message.js';
+import mongoose from 'mongoose';
+configDotenv();
 export function initializeSocket(app) {
+    async function checkAuthorization(socket) {
+        const cookies = cookie.parse(socket.request.headers.cookie);
+        const jwtCookie = cookies.jwt;
+        const jwtArr = jwtCookie.split(' ');
+        const authToken = jwtArr[1];
+        try {
+            const decode = jwt.verify(authToken, process.env.TOKEN_SECRET);
+
+            return decode;
+        } catch (e) {
+            console.error(e);
+            return null;
+        }
+    }
     const server = new createServer(app);
     const io = new Server(server, {
         cors: {
@@ -10,28 +32,32 @@ export function initializeSocket(app) {
         },
     });
 
+    io.use(async (socket, next) => {
+        const decode = await checkAuthorization(socket);
+        if (!decode) {
+            const error = new Error('unauthorized');
+            return next(error);
+        }
+
+        const user = await User.findById(decode.id, { password: 0 });
+        if (!user) {
+            const error = new Error('not found');
+            return next(error);
+        }
+
+        socket.request.user = user;
+        return next();
+    });
     io.on('connection', (socket) => {
-        // console.log(socket.id);
-        // console.log(socket.id);
-        // io.to(socket.id).emit('update', 'Update for a certain data');
-        console.log('connecting to socket');
-        console.log(socket.request.headers);
-        // console.log('user has connected');
-        socket.on('join', (roomId) => {
+        // Set a room with user id to be able to send messages to the user
+        const user = socket.request.user;
+        socket.join(user._id);
+
+        socket.on('join', async (roomId) => {
+            // Join a user to an id in this case im using
+            //  the chat id sent from the frondend
+            console.log('user joined a room');
             socket.join(roomId);
-            console.log('Someone join room : ' + roomId);
-            // io.to(roomId).emit('message', {
-            //     _id: 'post101',
-            //     user: {
-            //         username: 'john_doe',
-            //     },
-            //     text: 'Just had a great day at the park!',
-            //     date: '2024-09-04T10:30:00Z',
-            //     imgs: [
-            //         'https://example.com/images/park1.jpg',
-            //         'https://example.com/images/park2.jpg',
-            //     ],
-            // },)
         });
 
         socket.on('edit', (roomId, message) => {
@@ -46,12 +72,18 @@ export function initializeSocket(app) {
             io.to(roomId).emit('delete', message);
         });
 
-        socket.on('message', (roomId, text) => {
-            console.log(socket.request);
+        socket.on('message', async (roomId, text) => {
             console.log('Message from roomId: ' + roomId + ' text: ' + text);
-            socket.broadcast.to(roomId).emit('foo', text);
-            // io.emit('foo',text)
-            console.log(socket.id);
+            const user = socket.request.user;
+            const message = await new Message({
+                chatId: roomId,
+                text: text,
+                user: user._id,
+            }).save();
+
+            await message.populate('user');
+
+            io.to(roomId).emit('message', message);
         });
     });
 
